cc9ca131ab457ff194431e76460326f0b1380630
RIDWAN RAJI <ror716@mail.usask.ca>
2020-10-09 03:51:15 -0600

    nave and footer component

---
+++ src/components/PageBorders.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import "../styles/components/PageBorders.css";
+
+function PageBoders() {
+  return (
+    <>
+      <div className="page-borders">
+        <div className="page-borders-container">
+          <Navbar />
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PageBoders;
