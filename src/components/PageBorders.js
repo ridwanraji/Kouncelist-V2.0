253de53b281c90b69aeab7cbf0f5ad48c95d9cfb
RIDWAN RAJI <ror716@mail.usask.ca>
2020-10-12 22:16:38 -0600

    render intro component to screen

--- src/components/PageBorders.js
+++ src/components/PageBorders.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import "../styles/components/PageBorders.css";
+import IntroSection from "./IntroSection";
 
 function PageBoders() {
   return (
@@ -9,6 +10,7 @@ function PageBoders() {
       <div className="page-borders">
         <div className="page-borders-container">
           <Navbar />
+          <IntroSection />
           <Footer />
         </div>
       </div>
