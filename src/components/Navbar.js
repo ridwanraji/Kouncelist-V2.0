d4899da5fee3720dc3a44aacb6bfb43967f6693a
RIDWAN RAJI <ror716@mail.usask.ca>
2020-10-17 16:52:00 -0600

    changing navbar logo asset

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/components/Navbar.css";
-import logo from "../assets/Logo.svg";
+import logo from "../assets/Logo1.svg";
 
 function Navbar() {
   return (
